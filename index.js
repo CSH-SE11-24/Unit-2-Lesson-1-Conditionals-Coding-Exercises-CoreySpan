1461230382f4cd2ce59abd1e0418cdea1364ad69
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 18:11:11 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,66 @@
+// Exercise 1: Age Checker
+// Ask the user to enter their age. If they are 18 or older, display "You are an adult." Otherwise, display "You are not an adult."
+let age = prompt("How are you?")
+
+if (age >= 18) {
+  console.log("Your are an adult")
+}else if (age < 18) {
+  console.log("Your are not an adult")
+}
+
+
+
+
+// Exercise 2: Simple Comparison
+// Write a program that prompts the user to enter two numbers and displays whether the first number is greater, less than, or equal to the second number.
+// Note: make sure you have the correct types!
+let one = prompt("Enter number 1")
+let two = prompt("Enter number 2")
+
+if (one > two) {
+  console.log("Number 1 is bigger")
+}else if (one < two) {
+  console.log("Number 2 is bigger")
+}else (one >= two) 
+  console.log("Your numbers are equal")
+
+
+
+// Exercise 3: Even or Odd
+// Create a program that asks the user for an integer and then prints whether the number is even or odd. (Google how to check this!)
+let int = prompt("Enter an integer")
+
+if (int % 2 == 0) {
+  console.log(int + " is even.");
+} else {
+  console.log(int + " is odd.");
+}
+
+
+// Exercise 4: Day of the Week
+// Prompt the user to enter a day of the week (e.g., "Monday"). Display a message that says whether it's a weekday or the weekend.
+let day = prompt("Pick a day of the week")
+
+let week = [Monday, Tuesday, Wednesday, Thursday, Friday,]
+let weekend = [Sunday, Saturday]
+
+
+
+// Exercise 5: Age Group Classifier
+// Prompt the user to enter their age and categorize them into different age groups:
+// - 0-12: Child
+// - 13-19: Teenager
+// - 20-64: Adult
+// - 65+: Senior Citizen
+
+
+
+
+
+// Exercise 6: Rock, Paper, Scissors Game
+// Create a simple rock, paper, scissors game. Prompt the user to enter their choice (rock, paper, or scissors) and then randomly generate the computer's choice. Determine the winner and display the result.
+
+
+
+
+
